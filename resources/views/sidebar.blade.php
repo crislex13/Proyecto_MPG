4a9c520f6240aae085246fc26d58eb564b5ba271
Deymar100 <krn100dha.dha@gmail.com>
2025-02-18 19:06:21 -0400

    falta la correcion

--- resources/views/sidebar.blade.php
+++ resources/views/sidebar.blade.php
@@ -14,48 +14,58 @@
                 </div>
             </div>
             <ul class="navigation">
-                <li>
+                <li class="{{ request()->is('home') ? 'active' : '' }}">
                     <a href="{{ url('/home') }}">
                         <i class="text-primary menu-icon fa fa-fw fa-dashboard"></i>
                         <span class="mm-text">Panel de Control</span>
+                        @if(request()->is('home'))
+                            <span class="arrow"></span>
+                        @endif
                     </a>
                 </li>
-                <li>
-                    <a href='{{ route('clients.index')}}'>
+                <li class="{{ request()->is('clients*') ? 'active' : '' }}">
+                    <a href="{{ route('clients.index') }}">
                         <i class="text-primary menu-icon fa fa-th fa-info-circle"></i>
                         <span class="mm-text">Clientes</span>
+                        @if(request()->is('clients*'))
+                            <span class="arrow"></span>
+                        @endif
                     </a>
                 </li>
-                
-                <li class="menu-dropdown">
+                <li class="menu-dropdown {{ request()->is('users*') ? 'active' : '' }}">
                     <a href="#">
                         <i class="text-default menu-icon fa fa-fw fa-users"></i>
                         <span class="mm-text">Usuarios</span>
-                        <span class="fa fa-angle-down pull-right"></span>
+                        <!-- Cambiar la flecha según la ruta -->
+                        <span class="fa fa-angle-down pull-right {{ request()->is('users*') ? 'rotate' : '' }}"></span>
                     </a>
-                    <ul class="sub-menu">
-                        <li>
-                            <a href='{{ route('users.index')}}'>
+                    <ul class="sub-menu {{ request()->is('users*') ? 'show' : '' }}">
+                        <li class="{{ request()->is('users') ? 'active' : '' }}">
+                            <a href="{{ route('users.index') }}">
                                 <i class="text-primary fa fa-fw fa-users"></i> Lista de Usuarios
+                                @if(request()->is('users*'))
+                                    <span class="arrow"></span>
+                                @endif
                             </a>
                         </li>
-                        <li>
-                            <a href='admin_userprofile.html'>
+                        <li class="{{ request()->is('admin_userprofile') ? 'active' : '' }}">
+                            <a href="admin_userprofile.html">
                                 <i class="text-success fa fa-fw fa-user"></i> Perfil de Usuario
                             </a>
                         </li>
-                        <li>
-                            <a href='add_users.html'>
+                        <li class="{{ request()->is('add_users') ? 'active' : '' }}">
+                            <a href="add_users.html">
                                 <i class="text-info fa fa-fw fa-user"></i> Agregar Usuarios
                             </a>
                         </li>
-                        <li>
-                            <a href='admin_userpayment.html'>
+                        <li class="{{ request()->is('admin_userpayment') ? 'active' : '' }}">
+                            <a href="admin_userpayment.html">
                                 <i class="text-danger fa fa-fw fa-money"></i> Pagos
                             </a>
                         </li>
                     </ul>
                 </li>
+
                 <li>
                     <a href='admin_coupon.html'>
                         <i class="text-primary menu-icon fa fa-scissors"></i>
@@ -150,3 +160,33 @@
         </div>
     </section>
 </aside>
+<script>
+    document.addEventListener("DOMContentLoaded", function () {
+        // Seleccionamos todos los elementos <li> dentro del menú
+        let menuItems = document.querySelectorAll("li");
+
+        // Recorremos los elementos y les agregamos un evento de clic
+        menuItems.forEach(item => {
+            item.addEventListener("click", function () {
+                // Removemos la clase 'active' de todos los elementos
+                menuItems.forEach(el => el.classList.remove("active"));
+
+                // Agregamos la clase 'active' al elemento clickeado
+                this.classList.add("active");
+
+                // Opcional: Guardar en localStorage para recordar la selección después de recargar
+                localStorage.setItem("activeMenu", this.innerHTML);
+            });
+        });
+
+        // Recuperar el elemento activo al recargar la página
+        let savedMenu = localStorage.getItem("activeMenu");
+        if (savedMenu) {
+            menuItems.forEach(el => {
+                if (el.innerHTML === savedMenu) {
+                    el.classList.add("active");
+                }
+            });
+        }
+    });
+</script>
